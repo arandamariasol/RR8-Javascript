2910b54eadbefdd0bf2a9f3f2ddfa93ed68b628b
Maria Sol Aranda <arandamariasol@gmail.com>
2021-03-24 20:13:11 -0300

    1) Variables and Operators

---
+++ index.js
@@ -0,0 +1,25 @@
+// 1) Variables and Operators
+
+/* Create two numeric variables and use the sum operator to save
+the value of the sum of both numbers in a 3rd variable.*/
+
+var num1 = 2;
+var num2 = 5;
+var sumnum = num1 + num2;
+console.log('1) a) ' + sumnum);
+
+/* Create two  String variables and concatenate them saving the result
+in a 3rd variable. */
+
+var str1 = 'Maria ';
+var str2 = 'Sol';
+var name = str1.concat(str2);
+console.log('1) b) ' + name);
+
+/* Create two String variables  and sum the length of each variable (number of letters in the string)
+saving the result the sum of a 3rd variable (use length).*/
+
+var str3 = 'firstword';
+var str4 = 'secondword';
+var letters = str3.length + str4.length;
+console.log('1) c) ' + letters);
